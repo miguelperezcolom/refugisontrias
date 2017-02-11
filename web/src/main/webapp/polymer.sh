7ab43a069915aebf68484bf525071ec2cef4a4b6
miguelperezcolom <miguelperezcolom@gmail.com>
2017-02-11 18:18:53 +0100

    Initial commit

---
+++ web/src/main/webapp/polymer.sh
@@ -0,0 +1,9 @@
+bower install --save Polymer/polymer#^1.2.0
+bower install --save PolymerElements/iron-elements
+bower install --save PolymerElements/paper-elements
+bower install --save PolymerElements/gold-elements
+bower install --save PolymerElements/neon-elements
+bower install --save PolymerElements/platinum-elements
+bower install --save GoogleWebComponents/google-web-components
+bower install --save vaadin-core-elements
+bower install paper-alert-dialog
